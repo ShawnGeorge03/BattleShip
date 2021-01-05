0f789cb39539532079da82c91fa1c6ae87d4a238
Shawn Santhoshgeorge <shawngeorge2001@gmail.com>
2021-01-05 18:16:16 -0500

    FInal version of the BattleShip Game

---
+++ Game.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Game {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+
+        boolean isGameOver = false;
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+
+        System.out.println("Player 1, place your ships on the game field");
+        player1.battlefield.setupBoard();
+        changeTurn();
+
+        System.out.println("Player 2, place your ships on the game field");
+        player2.battlefield.setupBoard();
+        changeTurn();
+
+        //Until the game is over
+        while (!isGameOver) {
+            //printing the enemy field with the fog of war
+            player2.battlefield.printBattlefield(true);
+            System.out.println("---------------------");
+            //printing the current player's field without the fog of war
+            player1.battlefield.printBattlefield(false);
+            System.out.println("Player 1, it's your turn:");
+            //make turn
+            isGameOver = player2.battlefield.makeShot();
+            if (isGameOver) {
+                break;
+            }
+            changeTurn();
+            //printing the enemy field with the fog of war
+            player1.battlefield.printBattlefield(true);
+            System.out.println("---------------------");
+            //printing the current player's field without the fog of war
+            player2.battlefield.printBattlefield(false);
+            System.out.println("Player 2, it's your turn:");
+            //make turn
+            isGameOver = player1.battlefield.makeShot();
+            if (isGameOver) {
+                break;
+            }
+            changeTurn();
+        }
+        scanner.close();
+    }
+
+    /**
+     * Changing the move while waiting for the ENTER key to be pressed
+     */
+    public void changeTurn() {
+        System.out.println("Press Enter and pass the move to another player");
+        scanner.nextLine();
+    }
+}
